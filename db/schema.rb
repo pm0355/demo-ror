fe71d992d3758750a80adc94cd52313f6729a273
pm0355 <mattioli.paul@gmail.com>
2018-05-25 22:43:51 -0400

    commit to revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_26_014912) do
+
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "first_name", limit: 25
+    t.string "last_name", limit: 50
+    t.string "email", limit: 100, default: "", null: false
+    t.string "username", limit: 25
+    t.string "password", limit: 40
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
